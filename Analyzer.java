cb5d295ad0ba044fb0d6ecce7222d14510f0633a
aidan <aidanb363@gmail.com>
2022-11-28 14:57:50 -0500

    just setting up

---
+++ Analyzer.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Analyzer {
+    public int analyze(List<Integer> list){
+        
+    }
+
+    private int opt(int i){
+
+    }
+
+    private List<Integer> traceback(int i){
+
+    }
+}
